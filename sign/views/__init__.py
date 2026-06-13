52bed746e1b4e1797992bdf85e01a7f544714a6e
gustavolaires <gustavolaires@hotmail.com>
2026-06-13 17:34:13 -0300

    refactor(sign): separa views por recurso em produtos e fabricantes
    Converte sign/views.py no pacote sign/views/, com módulos products.py e manufacturers.py. O __init__.py reexporta as classes, mantendo a compatibilidade com `from . import views` e as rotas em urls.py.

---
+++ sign/views/__init__.py
@@ -0,0 +1,25 @@
+from .manufacturers import (
+    ManufacturerCreateView,
+    ManufacturerDeleteView,
+    ManufacturerListView,
+    ManufacturerUpdateView,
+)
+from .products import (
+    ProductCreateView,
+    ProductDeleteView,
+    ProductDetailView,
+    ProductListView,
+    ProductUpdateView,
+)
+
+__all__ = [
+    "ManufacturerCreateView",
+    "ManufacturerDeleteView",
+    "ManufacturerListView",
+    "ManufacturerUpdateView",
+    "ProductCreateView",
+    "ProductDeleteView",
+    "ProductDetailView",
+    "ProductListView",
+    "ProductUpdateView",
+]
--- sign/views/manufacturers.py
+++ sign/views/manufacturers.py
@@ -2,67 +2,16 @@ from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models import ProtectedError
 from django.shortcuts import redirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
     DeleteView,
-    DetailView,
     ListView,
     UpdateView,
 )
 
-from .forms import ManufacturerForm, ProductForm
-from .models import Manufacturer, Product
-
-
-# --- Produtos ---------------------------------------------------------------
-
-
-class ProductListView(ListView):
-    model = Product
-    template_name = "sign/products/list.html"
-    context_object_name = "products"
-
-    def get_queryset(self):
-        return Product.objects.select_related("manufacturer")
-
-
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "sign/products/detail.html"
-    context_object_name = "product"
-
-
-class ProductCreateView(SuccessMessageMixin, CreateView):
-    model = Product
-    form_class = ProductForm
-    template_name = "sign/products/form.html"
-    success_url = reverse_lazy("sign:product_list")
-    success_message = "Produto criado com sucesso."
-
-
-class ProductUpdateView(SuccessMessageMixin, UpdateView):
-    model = Product
-    form_class = ProductForm
-    template_name = "sign/products/form.html"
-    success_message = "Produto atualizado com sucesso."
-
-    def get_success_url(self):
-        return reverse("sign:product_detail", kwargs={"pk": self.object.pk})
-
-
-class ProductDeleteView(DeleteView):
-    model = Product
-    template_name = "sign/products/confirm_delete.html"
-    success_url = reverse_lazy("sign:product_list")
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, "Produto excluído com sucesso.")
-        return response
-
-
-# --- Fabricantes ------------------------------------------------------------
+from ..forms import ManufacturerForm
+from ..models import Manufacturer
 
 
 class ManufacturerListView(ListView):
